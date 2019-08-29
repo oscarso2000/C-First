c201270c2291c9047da46552b4c4557478a1a407
Oscar <os2304@columbia.edu>
2019-08-29 12:57:06 -0400

    Updated args

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main(int argc, char *argv[]){
 	cout<<"\nI will be using Mac Vim with g++ for this course \nBecause I want to prepare for CS3410.\n"<<endl;
 	return 0;
 }
